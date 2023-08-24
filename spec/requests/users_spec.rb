4c935b0ce185f832972425533a4b2403090ed3f9
Salomon Tshobohwa <stshobohwa@gmail.com>
2023-08-24 11:54:09 +0200

    Fix linters errors

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -3,16 +3,16 @@ require 'rails_helper'
 RSpec.describe 'Users', type: :request do
   describe 'GET #index' do
     it 'returns a successful response' do
-      get '/users'  
+      get '/users'
       expect(response).to be_successful
     end
 
     it 'renders the index template' do
-      get '/users'  
+      get '/users'
       expect(response).to render_template(:index)
     end
     it 'includes the template content in the response body' do
-      get '/users'  
+      get '/users'
       expect(response.body).to include('<h1>Here is the list of all the users</h1>')
     end
   end
