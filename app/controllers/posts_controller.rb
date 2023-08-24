6d128642f479745f4b1183ae0b0a22ffe8cd44d9
Salomon Tshobohwa <stshobohwa@gmail.com>
2023-08-24 16:26:23 +0200

    Query data from databases

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,17 @@
 class PostsController < ApplicationController
-  def index; end
+  def index
+    user_id = params[:user_id]
+    @user = User.find(user_id)
+    @posts = Post.where({ author_id: user_id })
+  end
 
-  def show; end
+  def show
+    id = params[:id]
+    author_id = params[:user_id]
+    @post_author = User.find(author_id).name
+    @post = Post.find(id)
+    @likes = @post.likes_counter
+    @comments_count = @post.comments_counter
+    @comments = Comment.where(post_id: id)
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,11 @@
 class UsersController < ApplicationController
-  def index; end
+  def index
+    @users = User.all
+  end
 
-  def show; end
+  def show
+    id = params[:id].to_i
+    @user = User.find(id)
+    @posts = @user.recent_posts
+  end
 end
