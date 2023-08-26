e864ede420bd162ea5b816ce3ccb584c31ab5b91
Salomon Tshobohwa <stshobohwa@gmail.com>
2023-08-26 07:17:29 +0200

    Add routes for creating posts, comments and likes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
-Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+# frozen_string_literal: true
 
-  # Defines the root path route ("/")
-  root "users#index"
-  resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
-  end
+Rails.application.routes.draw do
+  root to: "users#index"
+  get "/users/:id", to: "users#show"
+  get "/users/:id/posts", to: "posts#index"
+  get "users/:author_id/posts/new", to: "posts#new"
+  post "users/:author_id/posts", to: "posts#create"
+  get "/users/:author_id/posts/:id", to: "posts#show"
+  get "users/:author_id/posts/:id/comments/new", to: "comments#new"
+  post "users/:author_id/posts/:id/comments", to: "comments#create"
+  post "users/:author_id/posts/:id", to: "likes#create", as: "like_post"
+
+  resources :posts, only: [:index, :show, :new]
 end
